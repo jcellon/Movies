e006542f5b07ed6cca6b51ec7eaca9b85c7f29dd
Jacman <jcellon@outlook.com>
2022-01-23 14:11:49 -0500

    Movie Plots App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Icon, Menu, Table } from "semantic-ui-react";
+import movies from "./movie_plots.json";
+import EasyEdit from "react-easy-edit";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const save = (value) => {
+  alert(value);
+};
+const cancel = () => {
+  alert("Cancelled");
+};
 
-export default App;
+const Movies = () => (
+  <Table celled>
+    <Table.Header>
+      <Table.Row>
+        <Table.HeaderCell>Release Year</Table.HeaderCell>
+        <Table.HeaderCell>Title</Table.HeaderCell>
+        <Table.HeaderCell>Director</Table.HeaderCell>
+        <Table.HeaderCell>Genre</Table.HeaderCell>
+        <Table.HeaderCell>Wiki Page</Table.HeaderCell>
+      </Table.Row>
+    </Table.Header>
+
+    {movies.map((film) => {
+      return (
+        <Table.Body>
+          <Table.Row>
+            <Table.Cell>
+              <EasyEdit
+                type="text"
+                onSave={save}
+                onCancel={cancel}
+                placeholder={`${film.Release}`}
+              />
+            </Table.Cell>
+            <Table.Cell>
+              <EasyEdit
+                type="text"
+                onSave={save}
+                onCancel={cancel}
+                placeholder={`${film.Title}`}
+              />
+            </Table.Cell>
+            <Table.Cell>
+              <EasyEdit
+                type="text"
+                onSave={save}
+                onCancel={cancel}
+                placeholder={`${film.Director}`}
+              />
+            </Table.Cell>
+            <Table.Cell>
+              <EasyEdit
+                type="text"
+                onSave={save}
+                onCancel={cancel}
+                placeholder={`${film.Genre}`}
+              />
+            </Table.Cell>
+            <Table.Cell>
+              <EasyEdit
+                type="text"
+                onSave={save}
+                onCancel={cancel}
+                placeholder={`${film.Wiki}`}
+              />
+            </Table.Cell>
+          </Table.Row>
+        </Table.Body>
+      );
+    })}
+
+    <Table.Footer>
+      <Table.Row>
+        <Table.HeaderCell colSpan="5">
+          <i
+            style={{
+              color: "green",
+              paddingTop: "8em",
+            }}
+          >
+            Click in Cell to Edit
+          </i>
+          <Menu floated="right" pagination>
+            <Menu.Item as="a" icon>
+              <Icon name="chevron left" />
+            </Menu.Item>
+            <Menu.Item as="a">1</Menu.Item>
+            <Menu.Item as="a">2</Menu.Item>
+            <Menu.Item as="a" icon>
+              <Icon name="chevron right" />
+            </Menu.Item>
+          </Menu>
+        </Table.HeaderCell>
+      </Table.Row>
+    </Table.Footer>
+  </Table>
+);
+
+export default Movies;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Container } from "semantic-ui-react";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import pkg from "semantic-ui-react/package.json";
+import App from "./App";
+
+const Plot = ({ children }) => (
+  <Container style={{ margin: 20 }}>{children}</Container>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const styleLink = document.createElement("link");
+styleLink.rel = "stylesheet";
+styleLink.href =
+  "https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css";
+document.head.appendChild(styleLink);
+
+ReactDOM.render(
+  <Plot>
+    <App />
+  </Plot>,
+  document.getElementById("root")
+);
